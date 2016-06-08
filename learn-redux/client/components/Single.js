8715272c8dd21c57de70b60f6373800f88bea9a0
John Hilts <johnhilts@live.com>
2016-06-07 18:36:48 -0700

    Video 13
    Re-use a component
    use .findIndex on an array
    use the properties passed in, plus pass properties on to sub-component

--- learn-redux/client/components/Single.js
+++ learn-redux/client/components/Single.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {Link} from 'react-router';
+import Photo from './Photo';
+// import comments
 
 const Single = React.createClass({
   render() {
+    // index of the post
+    const i = this.props.posts.findIndex((post) => post.code === this.props.params.postId);
+    // get us the post
+    const post = this.props.posts[i];
+
     return (
       <div className="single-photo">
-        I'm the single
+        <Photo i={i} post={post} {...this.props} />
       </div>
     )
   }
