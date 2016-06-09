d5544ed3941c2444c8a54dd56baae814c4e42f3d
John Hilts <johnhilts@live.com>
2016-06-08 19:33:17 -0700

    Video 18
    Reducer hot update

--- learn-redux/client/store.js
+++ learn-redux/client/store.js
@@ -25,4 +25,12 @@ const store = createStore(rootReducer, defaultState);
 
 export const history = syncHistoryWithStore(browserHistory, store);
 
+// hot update reducers!
+if(module.hot) {
+  module.hot.accept('./reducers/', () => {
+    const nextRootReducer = require('./reducers/index').default;
+    store.replaceReducer(nextRootReducer);
+  })
+}
+
 export default store;
